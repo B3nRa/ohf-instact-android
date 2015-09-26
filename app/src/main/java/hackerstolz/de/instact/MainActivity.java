dffd11e29c0e5461a5c213c6e143dc90b054fd19
Benjamin Raethlein <benjamin.raethlein@gmail.com>
2015-09-26 16:46:48 +0200

    Changed CardView to support.CardView

--- app/src/main/java/hackerstolz/de/instact/MainActivity.java
+++ app/src/main/java/hackerstolz/de/instact/MainActivity.java
@@ -6,6 +6,8 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -15,6 +17,7 @@ import android.view.ViewGroup;
 
 import com.astuetz.PagerSlidingTabStrip;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 import java.util.UUID;
@@ -44,12 +47,15 @@ public class MainActivity extends AppCompatActivity {
 
     private P2pKitDataProvider p2pDataProvider;
 
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
 
         // Create the adapter that will return a fragment for each of the three
         // primary sections of the activity.
@@ -62,6 +68,21 @@ public class MainActivity extends AppCompatActivity {
         // Bind the tabs to the ViewPager
         PagerSlidingTabStrip tabs = (PagerSlidingTabStrip) findViewById(R.id.tabs);
         tabs.setViewPager(mViewPager);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        // specify an adapter (see also next example)
+        List<String> u = new ArrayList<String>();
+        u.add("Foobar");
+        mAdapter = new ContactListView(u);
+        mRecyclerView.setAdapter(mAdapter);
 
         p2pDataProvider = new P2pKitDataProvider(this, new P2pConnectionListener());
         p2pDataProvider.init();    }
