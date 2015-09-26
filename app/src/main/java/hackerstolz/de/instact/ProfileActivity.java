f50fc74fd29902b66314efb07cf99ad7b5f9f820
Henning Mu <muszynski.henning@gmail.com>
2015-09-26 20:05:20 +0200

    finished login activity and started profile

---
+++ app/src/main/java/hackerstolz/de/instact/ProfileActivity.java
@@ -0,0 +1,9 @@
+package hackerstolz.de.instact;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by muszy on 26-Sep-15.
+ */
+public class ProfileActivity extends AppCompatActivity {
+}
